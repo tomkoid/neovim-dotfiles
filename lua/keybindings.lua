7fbf7d73856659c0435c56c7474f4d1b86da4d7d
Tomkoid <tomkoid@tomkoid.tk>
2022-07-31 12:49:13 +0200

    Updated keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -2,10 +2,12 @@ local function map(m, k, v)
     vim.keymap.set(m, k, v, { silent = true })
 end
 
-map('n', '<C-t>', ':NERDTreeToggle<CR>')
-map("n", "<C-s>", "<C-W>W")
-map("n", "<C-Left>", ":tabprevious<CR>")
-map("n", "<C-Right>", ":tabnext<CR>")
-map("n", "<C-b>", ":vnew term://zsh<CR>")
-map("t", "<Esc>", [[<C-\><C-n>]])
-map("n", "<F4>", ":nohlsearch<CR>")
+map('n', '<C-t>', ':NERDTreeToggle<CR>') -- Toggle NERDTree
+map("n", "<C-s>", "<C-W>W") -- Switch focus to next window
+map("n", "<C-Left>", ":tabprevious<CR>") -- Switch to previous tab
+map("n", "<C-Right>", ":tabnext<CR>") -- Switch to next tab
+map("n", "<C-b>", ":vnew term://zsh<CR>") -- Open new terminal
+map("t", "<Esc>", [[<C-\><C-n>]]) -- Enable ESC in terminal mode
+map("n", "<F4>", ":nohlsearch<CR>") -- Clear search highlighting
+map("n", "<C-h>", ":vertical resize +5<CR>") -- Increase window size by 5%
+map("n", "<C-l>", ":vertical resize -5<CR>") -- Decrease window size window by 5%
