341a5c5e6ca8f68a81e8b83956a73898ae0d3954
Tomkoid <tomkoid@tomkoid.tk>
2022-08-01 19:45:50 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# Store the current directory
+orig_dir=$PWD
+
+cd ~/.config/nvim
+
 # Install packer
 echo "Installing packer.."
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
@@ -6,4 +13,13 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 # Install plugins
 nvim '+source ~/.config/nvim/lua/settings.lua' '+source ~/.config/nvim/lua/plugins.lua' +PackerUpdate '+echo "You may now leave with :qa"'
 
+# Install and build coc.nvim
+cd ~/.local/share/nvim/site/pack/packer/start/coc.nvim/
+echo "Building coc.nvim.."
+yarn build
+echo "Installing coc.nvim.."
+yarn install
+
 echo "NeoVim setup complete!"
+
+cd $orig_dir
