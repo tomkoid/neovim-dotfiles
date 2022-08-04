03cf9ceb8aa62ae7d9ef2ca96b3eafb5f911af3d
Tomkoid <tomkoid@tomkoid.tk>
2022-08-04 21:55:12 +0200

    Dashboard now reads custom header from files

--- lua/dashboard.lua
+++ lua/dashboard.lua
@@ -1,12 +1,18 @@
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.startify")
+local home = os.getenv("HOME")
 
-dashboard.section.header.val = {
-[[ _____               _         _     _]],
-[[|_   _|__  _ __ ___ | | _____ (_) __| |]],
-[[  | |/ _ \| '_ ` _ \| |/ / _ \| |/ _` |]],
-[[  | | (_) | | | | | |   < (_) | | (_| |]],
-[[  |_|\___/|_| |_| |_|_|\_\___/|_|\__,_|]]
-}
+-- You can find styles in ~/.config/nvim/static
+local style = "pikachu"
+
+local function read_array(file)
+  local array = {}
+  for line in io.lines(file) do
+    table.insert(array, line)
+  end
+  return array
+end
+
+dashboard.section.header.val = read_array(home .. "/.config/nvim/static/" .. style .. ".cat")
 
 alpha.setup(dashboard.config)
