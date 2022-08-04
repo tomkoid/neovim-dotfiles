6663a55fdc1cf86c3220a27ddf521d276320a230
Tomkoid <tomkoid@tomkoid.tk>
2022-08-04 16:44:38 +0200

    Made better install script

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "===================================="
+echo " _   _ _____ _____     _____ __  __ "
+echo "| \ | | ____/ _ \ \   / /_ _|  \/  |"
+echo "|  \| |  _|| | | \ \ / / | || |\/| |"
+echo "| |\  | |__| |_| |\ V /  | || |  | |"
+echo "|_| \_|_____\___/  \_/  |___|_|  |_|"
+echo "===================================="
+
 error=false
 if [ ! -f "/usr/bin/node" ]
 then
@@ -24,8 +32,60 @@ then
   exit 1
 fi
 
+echo "1 - Arch / Arch based distributions"
+echo "2 - Fedora / Fedora based distributions"
+echo "3 - Debian / Debian based distributions"
+echo "4 - Gentoo / Gentoo based distributions"
+echo "5 - Termux"
+echp "6 - Other"
+
+read -p "Please enter your Linux distribution: " distribution
+
+echo "=========================="
+echo "1. Installing dependencies"
+echo "=========================="
+
+case $distribution in
+  1)
+    distribution_name="Arch"
+    sudo pacman -Syy ttf-ubuntu-font-family ttf-jetbrains-mono neovim git --noconfirm --needed || echo "Error when installing dependencies"
+    ;;
+  2)
+    distribution_name="Fedora"
+    sudo dnf install jetbrains-mono-fonts neovim git || echo "Error when installing dependencies"
+    ;;
+  3)
+    distribution_name="Debian"
+    sudo apt-get install ttf-ubuntu-font-family fonts-jetbrains-mono neovim git || echo "Error when installing dependencies"  
+    ;;
+  4)
+    distribution_name="Gentoo"
+    sudo emerge media-fonts/ubuntu-font-family media-fonts/jetbrains-mono app-editors/neovim dev-cvs/git || echo "Error when installing dependencies"
+    ;;
+  5)
+    distribution_name="Termux"
+    apt update -y && apt install nodejs yarn git lua-language-server -y || echo "Error when installing dependencies"
+    ;;
+  6)
+    distribution_name="Other"
+    echo "Please install dependencies manually with your package manager"
+    ;;
+esac
+
+echo "Done."
+
+echo "==================================="
+echo "2. Installing packer plugin manager"
+echo "==================================="
+
 # Store the current directory
 orig_dir=$PWD
+
+# Create config directory for NeoVim if it doesn't exist
+if [ ! -d "$HOME/.config/nvim" ]
+then
+  mkdir -p $HOME/.config/nvim
+fi
 
 cd ~/.config/nvim
 
@@ -34,8 +94,18 @@ echo "Installing packer.."
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-# Install plugins
+echo "Done."
+
+echo "========================="
+echo "3. Installing all plugins"
+echo "========================="
+
+# Launch NeoVim and install plugins
 nvim '+source ~/.config/nvim/lua/settings.lua' '+source ~/.config/nvim/lua/plugins.lua' +PackerUpdate '+echo "You may now leave with :qa"'
+
+echo "================================="
+echo "4. Build and install intellisense"
+echo "================================="
 
 # Install and build coc.nvim
 cd ~/.local/share/nvim/site/pack/packer/start/coc.nvim/
@@ -44,8 +114,11 @@ yarn build
 echo "Installing coc.nvim.. This may take a while."
 yarn install
 
-echo "NeoVim setup complete!"
+echo "NeoVim installation complete!"
 
-echo "You may need to install Ubuntu Font Family, JetBrains Mono and Nerd Fonts for emoji and symbols to work."
+if [ $distribution_name == "Other" ]
+then
+  echo "You may need to install Ubuntu Font Family, JetBrains Mono and Nerd Fonts for emoji and symbols to work."
+fi
 
 cd $orig_dir
