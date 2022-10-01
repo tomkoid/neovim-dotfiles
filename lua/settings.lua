e48050a117fef982821fb99cb59650e542264690
Tomkoid <tomkoid@tomkoid.tk>
2022-10-01 12:55:11 +0200

    Removed deprecated highlights in settings.lua

--- lua/settings.lua
+++ lua/settings.lua
@@ -44,20 +44,20 @@ o.mouse = "a"
 o.clipboard = "unnamedplus"
 
 -- For Neovide
-highlight.create("LineNr", { guifg = "#5b6268", ctermfg = 8, guibg = "#282c34", ctermbg = "none",  cterm = "none" }, false)
-highlight.create("VertSplit", { guifg = "#1c1f24", ctermfg = 0, guibg = "#5b6268", ctermbg = 8, cterm = "none" }, false)
-highlight.create("Statement", { guifg = "#98be65", ctermfg = 2, guibg = "none", ctermbg = "none", cterm = "none" }, false)
-highlight.create("Directory", { guifg = "#51afef", ctermfg = 4, guibg="none", ctermbg="none", cterm="none" }, false)
-highlight.create("NERDTreeClosable", { guifg = "#98be65", ctermfg = 2 }, false)
-highlight.create("NERDTreeOpenable", { guifg = "#5b6268", ctermfg = 8 }, false)
-highlight.create("Comment", { guifg = "#A6A6A6",  ctermfg = 4, guibg = "none", ctermbg = "none", cterm = "italic" }, false)
-highlight.create("Constant", { guifg = "#93B9F6", ctermfg = 12, guibg = "none", ctermbg = "none", cterm = "none" }, false)
-highlight.create("Special", { guifg = "#8DB2EE", ctermfg = 4, guibg = "none", ctermbg = "none", cterm = "none" }, false)
-highlight.create("Identifier", { guifg = "#5699af", ctermfg = 6, guibg = "none", ctermbg = "none", cterm = "none" }, false)
-highlight.create("PreProc", { guifg = "#c678dd", ctermfg = 5, guibg = "none", ctermbg = "none", cterm = "none" }, false)
-highlight.create("String", { guifg = "#7A8AD4", ctermfg = 12, guibg = "none", ctermbg = "none", cterm = "none" }, false)
-highlight.create("Number", { guifg = "#ff6c6b", ctermfg = 1, guibg = "none", ctermbg = "none", cterm = "none" }, false)
-highlight.create("Function", { guifg = "#ff6c6b", ctermfg = 1, guibg = "none", ctermbg = "none", cterm = "none" }, false)
+-- highlight.create("LineNr", { guifg = "#5b6268", ctermfg = 8, guibg = "#282c34", ctermbg = "none",  cterm = "none" }, false)
+-- highlight.create("VertSplit", { guifg = "#1c1f24", ctermfg = 0, guibg = "#5b6268", ctermbg = 8, cterm = "none" }, false)
+-- highlight.create("Statement", { guifg = "#98be65", ctermfg = 2, guibg = "none", ctermbg = "none", cterm = "none" }, false)
+-- highlight.create("Directory", { guifg = "#51afef", ctermfg = 4, guibg="none", ctermbg="none", cterm="none" }, false)
+-- highlight.create("NERDTreeClosable", { guifg = "#98be65", ctermfg = 2 }, false)
+-- highlight.create("NERDTreeOpenable", { guifg = "#5b6268", ctermfg = 8 }, false)
+-- highlight.create("Comment", { guifg = "#A6A6A6",  ctermfg = 4, guibg = "none", ctermbg = "none", cterm = "italic" }, false)
+-- highlight.create("Constant", { guifg = "#93B9F6", ctermfg = 12, guibg = "none", ctermbg = "none", cterm = "none" }, false)
+-- highlight.create("Special", { guifg = "#8DB2EE", ctermfg = 4, guibg = "none", ctermbg = "none", cterm = "none" }, false)
+-- highlight.create("Identifier", { guifg = "#5699af", ctermfg = 6, guibg = "none", ctermbg = "none", cterm = "none" }, false)
+-- highlight.create("PreProc", { guifg = "#c678dd", ctermfg = 5, guibg = "none", ctermbg = "none", cterm = "none" }, false)
+-- highlight.create("String", { guifg = "#7A8AD4", ctermfg = 12, guibg = "none", ctermbg = "none", cterm = "none" }, false)
+-- highlight.create("Number", { guifg = "#ff6c6b", ctermfg = 1, guibg = "none", ctermbg = "none", cterm = "none" }, false)
+-- highlight.create("Function", { guifg = "#ff6c6b", ctermfg = 1, guibg = "none", ctermbg = "none", cterm = "none" }, false)
 
 -- New window behavior
 o.splitbelow = true
