47c86f28bb05c0d205cf9d83ae2bda8521533d50
Tomkoid <tomkoid@tomkoid.tk>
2023-07-10 13:31:36 +0200

    add telescope after plugin

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,9 @@
+require('telescope').setup {
+  defaults = {
+    file_ignore_patterns = {},
+    file_previewer = require('telescope.previewers').cat.new,
+    sorting_strategy = 'ascending',
+    path_display = { 'absolute' },
+    highlight = "TelescopePreviewNormal",
+  }
+}
