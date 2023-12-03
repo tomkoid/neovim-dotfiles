2ad61ac307c3ec29af752661e428446de6727361
Tomkoid <tomkoid@tomkoid.tk>
2023-12-03 16:57:58 +0100

    format, update

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,61 +10,61 @@ local lspconfig = require("lspconfig")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 lspconfig.efm.setup({
-  init_options = { documentFormatting = true },
-  settings = {
-    rootMarkers = { ".git/" },
-    languages = {
-      lua = { require("efmls-configs.linters.luacheck"), require("efmls-configs.formatters.stylua") },
-      go = {
-        -- require("efmls-configs.linters.golangci_lint"),
-        require("efmls-configs.formatters.gofmt"),
-        require("efmls-configs.formatters.goimports"),
-        require("efmls-configs.formatters.golines"),
-        require("efmls-configs.formatters.eslint"),
-        require("efmls-configs.linters.eslint"),
-      },
-    },
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-          -- vim.cmd(":Format<CR>")
-        end,
-      })
-    end
-  end,
+	init_options = { documentFormatting = true },
+	settings = {
+		rootMarkers = { ".git/" },
+		languages = {
+			lua = { require("efmls-configs.linters.luacheck"), require("efmls-configs.formatters.stylua") },
+			go = {
+				-- require("efmls-configs.linters.golangci_lint"),
+				require("efmls-configs.formatters.gofmt"),
+				require("efmls-configs.formatters.goimports"),
+				require("efmls-configs.formatters.golines"),
+				require("efmls-configs.formatters.eslint"),
+				require("efmls-configs.linters.eslint"),
+			},
+		},
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+					-- vim.cmd(":Format<CR>")
+				end,
+			})
+		end
+	end,
 })
 
 vim.diagnostic.config({
-  virtual_text = false,
-  signs = true,
-  underline = true,
-  update_in_insert = true,
+	virtual_text = false,
+	signs = true,
+	underline = true,
+	update_in_insert = true,
 })
 
 local caps = vim.tbl_deep_extend(
-  'force',
-  vim.lsp.protocol.make_client_capabilities(),
-  require('cmp_nvim_lsp').default_capabilities(),
-  -- File watching is disabled by default for neovim.
-  -- See: https://github.com/neovim/neovim/pull/22405
-  { workspace = { didChangeWatchedFiles = { dynamicRegistration = true } } }
-);
+	"force",
+	vim.lsp.protocol.make_client_capabilities(),
+	require("cmp_nvim_lsp").default_capabilities(),
+	-- File watching is disabled by default for neovim.
+	-- See: https://github.com/neovim/neovim/pull/22405
+	{ workspace = { didChangeWatchedFiles = { dynamicRegistration = true } } }
+)
 
 lspconfig.lua_ls.setup({
-  settings = {
-    Lua = {
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { "vim" },
-      },
-    },
-  },
+	settings = {
+		Lua = {
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+		},
+	},
 })
 
 lspconfig.clangd.setup({})
@@ -72,36 +72,36 @@ lspconfig.clangd.setup({})
 lspconfig.rust_analyzer.setup({})
 
 lspconfig.tsserver.setup({
-  on_attach = function(client)
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
-  end,
+	on_attach = function(client)
+		client.resolved_capabilities.document_formatting = false
+		client.resolved_capabilities.document_range_formatting = false
+	end,
 })
 
 lspconfig.eslint.setup({})
 
 lspconfig.gopls.setup({
-  on_attach = function(client)
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
-  end,
-  cmd = { "gopls" },
-  filetypes = { "go", "gomod", "gowork", "tmpl" },
-  settings = { gopls = { completeUnimported = true, usePlaceholders = true, analyses = { unusedparams = true } } },
+	on_attach = function(client)
+		client.resolved_capabilities.document_formatting = false
+		client.resolved_capabilities.document_range_formatting = false
+	end,
+	cmd = { "gopls" },
+	filetypes = { "go", "gomod", "gowork", "tmpl" },
+	settings = { gopls = { completeUnimported = true, usePlaceholders = true, analyses = { unusedparams = true } } },
 })
 
 lspconfig.gdscript.setup({})
-lspconfig.nil_ls.setup {
-  autostart = true,
-  capabilities = caps,
-  settings = {
-    ['nil'] = {
-      testSetting = 42,
-      formatting = {
-        command = { "nixpkgs-fmt" },
-      },
-    },
-  },
-}
+lspconfig.nil_ls.setup({
+	autostart = true,
+	capabilities = caps,
+	settings = {
+		["nil"] = {
+			testSetting = 42,
+			formatting = {
+				command = { "nixpkgs-fmt" },
+			},
+		},
+	},
+})
 lspconfig.svelte.setup({})
 -- lspconfig.nixd.setup({})
