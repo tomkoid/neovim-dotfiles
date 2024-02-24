456b90f5d8d71308dde8c4ab41d0b52e250aabf1
Tomkoid <tomaszierl@outlook.com>
2024-02-24 19:34:49 +0100

    remaps: set tmux keybinds

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,5 +1,5 @@
 local function map(m, k, v)
-  vim.keymap.set(m, k, v, { silent = true })
+	vim.keymap.set(m, k, v, { silent = true })
 end
 
 --local telescope = require('telescope.builtin')
@@ -12,29 +12,35 @@ vim.g.maplocalleader = " "
 map("v", "J", ":m '>+1<cr>gv=gv")
 map("v", "K", ":m '<-2<cr>gv=gv")
 
-map("n", "<leader>lg", ":LazyGit<CR>")       -- open lazygit
+-- tmux
+map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
+map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
+map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>")
+map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>")
+
+map("n", "<leader>lg", ":LazyGit<CR>") -- open lazygit
 map("n", "<leader>t", ":NvimTreeToggle<cr>") -- open nvim tree
 --map("n", "<leader>ff", telescope.find_files) -- open telescope file manager
 --map("n", "<leader>fg", telescope.live_grep) -- open telescope live grep
 --map("n", "<leader>fb", telescope.buffers) -- open list of all buffers in telescope
 --map("n", "<leader>fh", telescope.help_tags) -- open help tags
 map("n", "<leader>ff", ":Telescope find_files<CR>") -- open telescope file manager
-map("n", "<leader>fg", ":Telescope live_grep<CR>")  -- open telescope file manager
-map("n", "<leader>fb", ":Telescope buffers<CR>")    -- open telescope file manager
-map("n", "<leader>fh", ":Telescope help_tags<CR>")  -- open telescope file manager
+map("n", "<leader>fg", ":Telescope live_grep<CR>") -- open telescope file manager
+map("n", "<leader>fb", ":Telescope buffers<CR>") -- open telescope file manager
+map("n", "<leader>fh", ":Telescope help_tags<CR>") -- open telescope file manager
 --map("n", "<leader>fb", telescope.buffers) -- open list of all buffers in telescope
 --map("n", "<leader>fh", telescope.help_tags) -- open help tags
-map("n", "<leader>s", "<c-w>w")              -- switch focus to next window
-map("n", "<c-left>", ":tabprevious<cr>")     -- switch to previous tab
-map("n", "<c-right>", ":tabnext<cr>")        -- switch to next tab
-map("n", "<c-b>", ":vnew term://zsh<cr>")    -- open new terminal
-map("t", "<esc>", [[<c-\><c-n>]])            -- enable esc in terminal mode
+map("n", "<leader>s", "<c-w>w") -- switch focus to next window
+map("n", "<c-left>", ":tabprevious<cr>") -- switch to previous tab
+map("n", "<c-right>", ":tabnext<cr>") -- switch to next tab
+map("n", "<c-b>", ":vnew term://zsh<cr>") -- open new terminal
+map("t", "<esc>", [[<c-\><c-n>]]) -- enable esc in terminal mode
 map("n", "d-v", "")
-map("n", "<f4>", ":nohlsearch<cr>")          -- clear search highlighting
+map("n", "<f4>", ":nohlsearch<cr>") -- clear search highlighting
 map("n", "<c-h>", ":vertical resize -5<cr>") -- increase window size by 5%
 map("n", "<c-l>", ":vertical resize +5<cr>") -- decrease window size window by 5%
-map("n", "<leader>e", "<esc>$")              -- go to the end of the line
-map("n", "<leader>a", "<esc>^")              -- go to the start of the line
+map("n", "<leader>e", "<esc>$") -- go to the end of the line
+map("n", "<leader>a", "<esc>^") -- go to the start of the line
 
 -- Trouble
 map("n", "<leader>rr", vim.lsp.buf.rename)
@@ -42,11 +48,11 @@ map("n", "<leader>ca", vim.lsp.buf.code_action)
 map("n", "<leader>gd", ":Trouble lsp_definitions<CR>")
 
 -- TABS
-map("n", "<leader>n", ":BufferNext<CR>")          -- next tab
-map("n", "<leader>mn", ":BufferMoveNext<CR>")     -- move to next
-map("n", "<leader>p", ":BufferPrevious<CR>")      -- previous tab
+map("n", "<leader>n", ":BufferNext<CR>") -- next tab
+map("n", "<leader>mn", ":BufferMoveNext<CR>") -- move to next
+map("n", "<leader>p", ":BufferPrevious<CR>") -- previous tab
 map("n", "<leader>mp", ":BufferMovePrevious<CR>") -- move previous
-map("n", "<leader>x", ":BufferClose<CR>")         -- close tab
+map("n", "<leader>x", ":BufferClose<CR>") -- close tab
 
 -- intellisense
 -- vim.api.nvim_set_keymap("i", "<tab>", "coc#pum#visible() ? coc#pum#next(1): '<tab>'", {noremap = true, silent = true, expr = true})
