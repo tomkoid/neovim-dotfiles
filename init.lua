c934f481f14a8dab401bd06265e5c3939efa6836
Tomkoid <tomaszierl@outlook.com>
2024-03-28 17:08:38 +0100

    init.lua: format and remove comment
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- init.lua
+++ init.lua
@@ -1,20 +1,19 @@
 local function is_module_available(name)
-  if package.loaded[name] then
-    return true
-  else
-    for _, searcher in ipairs(package.searchers or package.loaders) do
-      local loader = searcher(name)
-      if type(loader) == "function" then
-        package.preload[name] = loader
-        return true
-      end
-    end
-    return false
-  end
+	if package.loaded[name] then
+		return true
+	else
+		for _, searcher in ipairs(package.searchers or package.loaders) do
+			local loader = searcher(name)
+			if type(loader) == "function" then
+				package.preload[name] = loader
+				return true
+			end
+		end
+		return false
+	end
 end
 
 -- Load all my scripts
--- require("plugin-manager")
 require("remap")
 require("settings")
 require("plugins")
@@ -22,18 +21,18 @@ require("autocmd")
 require("neovide")
 require("colors")
 if is_module_available("alpha") then
-  require("dashboard")
+	require("dashboard")
 end
 if is_module_available("lualine") then
-  require("lualine_conf")
+	require("lualine_conf")
 end
 
 if is_module_available("lspconfig") then
-  require("lspconfig")
+	require("lspconfig")
 end
 
 ---Pretty print lua table
 function _G.dump(...)
-  local objects = vim.tbl_map(vim.inspect, { ... })
-  print(unpack(objects))
+	local objects = vim.tbl_map(vim.inspect, { ... })
+	print(unpack(objects))
 end
