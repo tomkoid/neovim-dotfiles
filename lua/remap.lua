897dc068c361c4cc581abb696a2ace4f1edb0343
Tomkoid <tomaszierl@outlook.com>
2024-05-04 11:43:31 +0200

    remap: add diagnostic jumping keybindings
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- lua/remap.lua
+++ lua/remap.lua
@@ -12,6 +12,12 @@ vim.g.maplocalleader = " "
 map("v", "J", ":m '>+1<cr>gv=gv")
 map("v", "K", ":m '<-2<cr>gv=gv")
 
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
 -- tmux
 map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
 map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
