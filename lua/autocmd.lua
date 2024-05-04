f24eeff306a40a62b32b6aa478220a58f10be61d
Tomkoid <tomaszierl@outlook.com>
2024-05-04 11:46:09 +0200

    autocmd: add autocmd to highlight when yanking text
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -3,3 +3,12 @@ vim.cmd("autocmd BufEnter * if winnr('$') == 1 && exists('b:NERDTree') && b:NERD
 
 -- Inline diagnostics
 vim.cmd("autocmd CursorHold * lua vim.diagnostic.open_float(nil, {focusable = false})")
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
