5bcc213ee84643621e02d8a48e18f76b5ec44a73
Tomkoid <tomaszierl@outlook.com>
2024-06-23 10:42:30 +0200

    lsp: use local clangd
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -202,6 +202,7 @@ return {
 			--    :Mason
 			--
 			--  You can press `g?` for help in this menu.
+			require("lspconfig").clangd.setup({})
 			require("mason").setup()
 
 			-- You can add other tools here that you want Mason to install
