260fe398eb660b575876ac2cfb23232a2df47c8c
Tomkoid <tomaszierl@outlook.com>
2024-07-28 23:16:11 +0200

    settings: use 2 shiftwidth and tabstop again
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- lua/set.lua
+++ lua/set.lua
@@ -73,7 +73,7 @@ vim.opt.hlsearch = true
 
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 
 vim.opt.laststatus = 3
