908497472cfceadb12de03d8aadad2f6129fb698
Tomkoid <tomaszierl@outlook.com>
2025-02-09 12:07:10 +0100

    chore: switch to tokyonight-night
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,11 +20,21 @@ return {
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("catppuccin-mocha")
+			-- vim.cmd.colorscheme("catppuccin-mocha")
 
 			-- You can configure highlights by doing something like:
-			vim.cmd.hi("Comment gui=none")
+			-- vim.cmd.hi("Comment gui=none")
 		end,
+	},
+
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		init = function()
+			vim.cmd [[colorscheme tokyonight-night]]
+		end
 	},
 
 	-- Highlight todo, notes, etc in comments
