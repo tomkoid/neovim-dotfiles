05bce6a0942ff77cf53c7a04b0d194fb9ba8914e
Tomkoid <tomaszierl@outlook.com>
2025-09-02 20:39:01 +0200

    chore: switch to catppuccin-mocha
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,27 +15,28 @@ return {
 	{
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 		"catppuccin/nvim",
+		name = "catppuccin",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			-- vim.cmd.colorscheme("catppuccin-mocha")
+			vim.cmd.colorscheme("catppuccin-mocha")
 
 			-- You can configure highlights by doing something like:
-			-- vim.cmd.hi("Comment gui=none")
+			vim.cmd.hi("Comment gui=none")
 		end,
 	},
 
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		init = function()
-			vim.cmd [[colorscheme tokyonight-night]]
-		end
-	},
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {},
+	-- 	init = function()
+	-- 		vim.cmd [[colorscheme tokyonight-night]]
+	-- 	end
+	-- },
 
 	-- Highlight todo, notes, etc in comments
 	{
@@ -96,7 +97,10 @@ return {
 	-- Hightlight Rust lifetimes
 	{
 		"cordx56/rustowl",
-		dependencies = { "neovim/nvim-lspconfig" }
+		version = '*', -- Latest stable version
+		build = 'cargo binstall rustowl',
+		lazy = false, -- This plugin is already lazy
+		opts = {},
 	},
 
 	{
