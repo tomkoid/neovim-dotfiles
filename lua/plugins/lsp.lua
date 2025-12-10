f00e633d157e34bedef3ccd7f18f21f81d7ec2f1
Tomkoid <tomaszierl@outlook.com>
2025-12-10 16:04:46 +0100

    fix: use vim.lsp.buf.definition to go to definition
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -72,7 +72,7 @@ return {
 					-- Jump to the definition of the word under your cursor.
 					--  This is where a variable was first declared, or where a function is defined, etc.
 					--  To jump back, press <C-t>.
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
 
 					-- Find references for the word under your cursor.
 					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
